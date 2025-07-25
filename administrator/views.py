f433f9c094f6789d57a3aa686e6f2106d8fae441
Niharika286 <niharika8708@gmail.com>
2025-07-25 12:43:53 +0530

    new

--- administrator/views.py
+++ administrator/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Administrator HOME PAGE")
+def register(request):
+    return HttpResponse("welcome to registration")
